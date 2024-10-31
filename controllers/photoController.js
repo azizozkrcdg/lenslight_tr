2f13e26ccc042cf6fb71c30d44c0a3aebb7642de
aziz özkaracadağ <azizozkaracadaag@gmail.com>
2024-10-31 20:13:10 +0300

    thunder client kullanıldı

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,11 +1,36 @@
 import Photo from "../models/photoModels.js";
 
-const createPhoto = (req, res) => {
-    const photo = Photo.create(req.body);
-    res.status(201).json({
+const createPhoto = async (req, res) => {
+    
+    try {
+        const photo = await Photo.create(req.body);
+    
+        res.status(201).json({
         succeded: true,
         photo
     });
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error
+        });
+    }
 }
 
-export {createPhoto};
+const getAllPhotos = async (req, res) => {
+    try {
+        const photos = await Photo.find({});
+        res.status(200).json({
+            succeded: true,
+            photos
+        });
+    } catch (error) {
+        res.status(500).json({
+            succeded: false,
+            error
+        });
+    }
+}
+
+
+export {createPhoto, getAllPhotos};
